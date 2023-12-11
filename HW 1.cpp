ed7028edce5d5a2eb77236f1c650da5b344a65c2
Ramtinsamsami <153230906+Ramtinsamsami@users.noreply.github.com>
2023-12-11 10:52:03 +0330

    Add files via upload

---
+++ HW 1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+main()   {
+	float x,y,s,t,p,g;
+	cout<<"please enter x number:";
+	cin>>x;
+	cout<<"please enter y number ";
+	cin>>y;
+	s=x+y;
+	cout<<"the result of x+y is : "<<s<<"\n";
+	t=x-y;
+	cout<<"the result of x-y is:"<<t<<"\n";
+	p=x*y;
+	cout<<"the result of x*y is:"<<p<<"\n";
+	g=x/y;
+	cout<<"the result of x/y is:"<<g<<"\n";
+}
